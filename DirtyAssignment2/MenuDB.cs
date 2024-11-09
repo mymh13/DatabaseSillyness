d89ff87560dd076da5a5db38f89d8b2f19359b56
Niklas H <156240330+mymh13@users.noreply.github.com>
2024-11-09 14:57:20 +0100

    added switch menuInput

--- DirtyAssignment2/MenuDB.cs
+++ DirtyAssignment2/MenuDB.cs
@@ -18,6 +18,30 @@ namespace DirtyAssignment2
             Console.WriteLine("5. Exit\n");
 
             Console.Write("Enter your choice: ");
+            
+            string? menuInput = Console.ReadLine();
+            switch (menuInput)
+            {
+                case "1":
+                    MongoDB.MongoDBMenu();
+                    break;
+                case "2":
+                    MySQL.MySQLMenu();
+                    break;
+                case "3":
+                    SQLite.SQLiteMenu();
+                    break;
+                case "4":
+                    API.APIMenu();
+                    break;
+                case "5":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please try again.\n");
+                    MenuChoices();
+                    break;
+            }
         }
     }
 }
